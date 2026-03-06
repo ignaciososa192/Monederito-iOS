2de292bc89007785773731a96a2a40f9a1572c81
Ignacio Sosa <ignacio.sosa92@gmail.com>
2026-03-06 19:18:21 -0300

    Initial Commit

---
+++ Monederito-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Monederito-iOS
+//
+//  Created by Ignacio Sosa on 06/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
