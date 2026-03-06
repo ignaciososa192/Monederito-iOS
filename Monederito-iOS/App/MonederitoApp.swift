9d18adcfdb851791b129e1378b29b50adbe06b1b
Ignacio Sosa <ignacio.sosa92@gmail.com>
2026-03-06 19:41:04 -0300

    feat: add domain layer - models, enums and repository protocols
    - Add UserRole, TransactionCategory, AlertStatus enums
    - Add User, Transaction, RiskAlert, BeneficiaryAccount models
    - Add AuthRepositoryProtocol and TransactionRepositoryProtocol
    - Add Color+Monederito extensions with brand palette
    - Add MockData for development without backend

---
+++ Monederito-iOS/App/MonederitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monederito_iOSApp.swift
+//  Monederito-iOS
+//
+//  Created by Ignacio Sosa on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Monederito_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Monederito-iOS/Domain/Models/RiskAlert.swift
@@ -0,0 +1,7 @@
+//
+//  Alert.swift
+//  Monederito-iOS
+//
+//  Created by Ignacio Sosa on 06/03/2026.
+//
+
